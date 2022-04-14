5360d97dfe937b9359422886d74a4e21de8559a0
Rodrigo <arcerodrigo.m@gmail.com>
2022-04-14 17:52:38 -0300

    inclusión de bibliotecas

---
+++ TP_[1]/tp_1/src/biblioteca_tp_1.c
@@ -0,0 +1,37 @@
+/*
+ * biblioteca_tp_1.c
+ *
+ *  Created on: 14 abr. 2022
+ *      Author: Paula Marchi
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int getNumero(int * pNumeroIngresado, char * mensaje, char * mensajeIncorrecto, int maximo, int minimo, int cantidadIntentos){
+int retorno;
+retorno = -1;
+float auxNumeroIngresado;
+
+if(pNumeroIngresado != NULL && maximo >= minimo && minimo <= maximo && cantidadIntentos > 0){
+
+do{
+	printf("%s", mensaje);
+	scanf("%f", &auxNumeroIngresado);
+	cantidadIntentos--;
+
+	if(auxNumeroIngresado >= minimo && auxNumeroIngresado <= maximo){
+
+		*pNumeroIngresado = auxNumeroIngresado;
+		retorno = 0;
+		break;
+	}
+	else{
+		printf("%s", mensajeIncorrecto);
+	}
+}while(cantidadIntentos > 0);
+
+}
+
+return retorno;
+}
+
---
+++ TP_[1]/tp_1/src/biblioteca_tp_1.h
@@ -0,0 +1,13 @@
+/*
+ * biblioteca_tp_1.h
+ *
+ *  Created on: 14 abr. 2022
+ *      Author: Paula Marchi
+ */
+
+#ifndef BIBLIOTECA_TP_1_H_
+#define BIBLIOTECA_TP_1_H_
+
+int getNumero(int * pNumeroIngresado, char * mensaje, char * mensajeIncorrecto, int maximo, int minimo, int cantidadIntentos);
+
+#endif /* BIBLIOTECA_TP_1_H_ */
