0d35d93f1482032eade332602afdf6062815030f
Rodrigo <arcerodrigo.m@gmail.com>
2022-04-16 19:23:55 -0300

    bibliotecas de calculos added

---
+++ TP_[1]/tp_1/src/calculos.c
@@ -0,0 +1,53 @@
+/*
+ * calculos.c
+ *
+ *  Created on: 16 abr. 2022
+ *      Author: Paula Marchi
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float calculosTarjetaDebito(float *dato1, int porcentaje){
+float resultado;
+float resultadoFinal;
+
+if(dato1 != NULL && porcentaje >0){
+
+resultado = *dato1 * porcentaje / 100;
+resultadoFinal = *dato1 - resultado;
+}
+	return resultadoFinal;
+}
+
+float calculosTarjetaCredito(float *dato1, int porcentaje){
+	float resultado;
+	float resultadoFinal;
+
+	if(dato1 != NULL && porcentaje >0){
+
+	resultado = *dato1 * porcentaje / 100;
+	resultadoFinal = *dato1 + resultado;
+	}
+		return resultadoFinal;
+	}
+
+float calculosBitcoin(float *dato1, float *dato3){
+	float resultadoFinal;
+
+	if(dato1 != NULL){
+	resultadoFinal = *dato1 / *dato3;
+}
+	return resultadoFinal;
+}
+
+float calculoDiferenciaDePrecio(float *dato1, float *dato2){
+	float resultadoFinal;
+
+	resultadoFinal = *dato1 - *dato2;
+	if(resultadoFinal < 0){
+
+		resultadoFinal = *dato2 - *dato1;
+	}
+	return resultadoFinal;
+}
---
+++ TP_[1]/tp_1/src/calculos.h
@@ -0,0 +1,16 @@
+/*
+ * calculos.h
+ *
+ *  Created on: 16 abr. 2022
+ *      Author: Paula Marchi
+ */
+
+#ifndef CALCULOS_H_
+#define CALCULOS_H_
+
+float calculosTarjetaDebito(float *dato1, int porcentaje);
+float calculosTarjetaCredito(float *dato1, int porcentaje);
+float calculosBitcoin(float *dato1, float *dato3);
+float calculoDiferenciaDePrecio(float *dato1, float *dato2);
+
+#endif /* CALCULOS_H_ */
