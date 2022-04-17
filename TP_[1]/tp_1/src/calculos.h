7e389b8c7b81908a19af31a4342b4711a3f48256
Rodrigo <arcerodrigo.m@gmail.com>
2022-04-16 22:20:52 -0300

    biblioteca documentada

--- TP_[1]/tp_1/src/calculos.h
+++ TP_[1]/tp_1/src/calculos.h
@@ -7,10 +7,35 @@
 
 #ifndef CALCULOS_H_
 #define CALCULOS_H_
-
+/// @param dato1
+/// @param porcentaje
+/// @return
+/// Descripción-
+/// Agrega 1 float y 1 Int.
+/// Su función es calcular el porcentaje de descuento
 float calculosTarjetaDebito(float *dato1, int porcentaje);
+/// @param dato1
+/// @param porcentaje
+/// @return
+/// Descripción-
+/// Agrega un float y un Int.
+/// Su función es calcular el porcentaje de interés
 float calculosTarjetaCredito(float *dato1, int porcentaje);
+/// @param dato1
+/// @param dato3
+/// @return
+/// Descripción-
+/// Agrega 2 float
+/// Su función es transformar el precio en pesos a Bitcoin
 float calculosBitcoin(float *dato1, float *dato3);
+/// @param dato1
+/// @param dato2
+/// @return
+/// Descripción-
+/// agrega 2 float
+/// Su función es calcular la diferencia de precios entre Aerolineas y Latam
 float calculoDiferenciaDePrecio(float *dato1, float *dato2);
 
 #endif /* CALCULOS_H_ */
+
+
